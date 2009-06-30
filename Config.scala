95bf905e67a6e4895411f0fd54d5a44050ea6419
Etienne Kneuss <colder@php.net>
2009-06-30 13:28:35 +0200

    Ship configuration to a specific class

---
+++ Config.scala
@@ -0,0 +1,22 @@
+package ircbot;
+
+import scala.xml._
+
+
+class Config(path: String) {
+    private val data = XML.loadFile(path)
+    private val db = data \ "db" first
+    val dbUser = (db \ "@user").text
+    val dbPass = (db \ "@pass").text
+    val dbDatabase = (db \ "@database").text
+
+    private val host = data \ "host" first
+    val hostHost = (host \ "@host").text
+    val hostPort = Integer.parseInt((host \ "@port").text)
+
+    private val auth = data \ "auth" first
+    val authNick = (auth \ "@nick").text
+    val authPass = (auth \ "@pass").text
+    val authRealName = (auth \ "@realname").text
+
+}
