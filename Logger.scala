38637ff75fb91195cf7f6005fc1dc6a113b6c7fa
Etienne Kneuss <colder@php.net>
2009-07-14 04:11:24 +0200

    Add Warnings to the Logger class

--- Logger.scala
+++ Logger.scala
@@ -7,19 +7,22 @@ abstract class Logger {
     def out(msg: String);
     def in(msg: String);
     def err(msg: String);
+    def warn(msg: String);
 }
 
 class TerminalLogger extends Logger {
     val df = new SimpleDateFormat("dd-MMM-yy HH:mm:ss")
     def date = df.format(new Date())
 
-    def out(msg: String) = println(date+" [>] "+msg)
-    def in(msg: String)  = println(date+" [<] "+msg)
-    def err(msg: String) = println(date+" [!] "+msg)
+    def out(msg: String)  = println(date+" [>] "+msg)
+    def in(msg: String)   = println(date+" [<] "+msg)
+    def err(msg: String)  = println(date+" [!] "+msg)
+    def warn(msg: String) = println(date+" [w] "+msg)
 }
 
 class TerminalColorLogger extends TerminalLogger {
     override def out(msg: String) = println(date+" \033[34m[>]\033[0m "+msg)
     override def in(msg: String)  = println(date+" \033[32m[<]\033[0m "+msg)
     override def err(msg: String) = println(date+" \033[31m[!]\033[0m "+msg)
+    override def warn(msg: String) = println(date+" \033[33m[w]\033[0m "+msg)
 }
