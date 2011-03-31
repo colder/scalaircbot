4c92f698d93ee6e6e507e32757eb00a4a99560cb
Chad Minick <cythrawll@codeangel.org>
2011-03-31 01:18:01 -0500

    added go task for production running

--- project/build/Scalaircbot.scala
+++ project/build/Scalaircbot.scala
@@ -1,7 +1,12 @@
 import sbt._
+import Process._
 
 class ScalaIRCBotProject(info: ProjectInfo) extends DefaultProject(info) {
 	override def mainClass: Option[String] = Some("ircbot.Main")
+	lazy val go = task { 
+		"scala -cp target/scala_2.8.1/classes:lib_managed/scala_2.8.1/compile/* ircbot.Main config-prod.xml 2>&1 | tee -a bot.log" !;
+	 	None 
+	} dependsOn(compile)
 
 	val codec = "commons-codec" % "commons-codec" % "1.3"
 	val httpclient = "commons-httpclient" % "commons-httpclient" % "3.1"
